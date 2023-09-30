e835aa8e147a796bfcf220f81293560192c2d128
Sasha <132772266+notpatern@users.noreply.github.com>
2023-09-30 15:01:37 +0200

    scene overall

---
+++ Assets/Scenes/SA/SACHA/Tp2sasha.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tp2sasha: MonoBehaviour
+{
+    public Transform Player;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("fimskdfjms");
+        Player.transform.position = new Vector3(45f, Player.transform.position.y, Player.transform.position.z);
+    }
+}
