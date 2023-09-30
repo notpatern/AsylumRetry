8b97af48ad04e85b5e2967229dece2f4f5ed4fe7
Sasha <132772266+notpatern@users.noreply.github.com>
2023-09-30 19:58:14 +0200

    rhrhrthr

--- Assets/Scenes/PI/SCRIPTS/Movement2D.cs
+++ Assets/Scenes/PI/SCRIPTS/Movement2D.cs
@@ -17,67 +17,65 @@ public class Movement2D : MonoBehaviour
     public float jumpCooldown;
     public float airMultiplier;
 
-    bool readyToJump;
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-        readyToJump = true;
         grounded = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        CheckGrounded();
-        if (Input.GetKey(KeyCode.Space) && readyToJump && grounded)
-        {
-            Debug.Log("Caca");
-            readyToJump = false;
-            Jump();
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
     }
 
     private void FixedUpdate()
     {
         Move();
+        Jump();
+        CheckGrounded();
     }
 
-    private void Move()
+    private void Update()
     {
-        if (grounded)
-        {
-            rb.drag = 5f;
-            if (moveSpeed < maxSpeed)
-            {
-                rb.AddForce(Vector3.right * moveSpeed * horizontalInput, ForceMode.Impulse);
-            }
-        }
-
-        else if (!grounded)
-            rb.drag = 0f;
-            rb.AddForce(Vector3.right * moveSpeed * airMultiplier * horizontalInput, ForceMode.Impulse);
-
+        MyInput();
+        StateMachine();
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
-    private void ResetJump()
+    private void MyInput()
     {
-        readyToJump = true;
+        if (moveSpeed < maxSpeed)
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+        }
     }
 
     private void CheckGrounded()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, 2 + 0.2f, isGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, 2f, isGround);
+    }
+
+    private void StateMachine()
+    {
+        if (grounded)
+        {
+            rb.drag = 5f;
+            airMultiplier = 1f;
+        }
+        if (!grounded)
+        {
+            rb.drag = 0f;
+            airMultiplier = 0.7f;
+        }
+    }
+
+    private void Move()
+    {
+            rb.AddForce(Vector3.right * moveSpeed * airMultiplier * horizontalInput, ForceMode.Impulse);
     }
 }
