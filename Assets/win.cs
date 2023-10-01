bca526da30a21db73a24a86634127848aa8b4188
PS1HAGRID <compteboubelle@gmail.com>
2023-10-01 19:14:40 +0200

    regiment

--- Assets/win.cs
+++ Assets/win.cs
@@ -1,20 +1,38 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class win : MonoBehaviour
 {
     TextMeshPro textmeshPro;
- 
+    float time; 
+    bool g;
     private void Start()
     {
         textmeshPro = GetComponent<TextMeshPro>();
         textmeshPro.text = " ";
+        g = false;
+        time = 3f;
+    }
+    private void Update()
+    {
+        if (g)
+        {
+            if (time >= 0)
+            {
+                time -= Time.deltaTime;
+            }
+            if (time < 0)
+            {
+                SceneManager.LoadScene("Corridor");
+            }
+        }
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
-        
         textmeshPro.text = "You Win!";
-
+        g = true;
     }
 
 }
