6a5f88b54974e37daf9aee88d0a5a45b3020108b
Porolito <pierre.thomas16380@gmail.com>
2023-10-02 16:30:53 +0200

    Trigger + TP Camera

--- Assets/Scenes/Snd_Trd/TrdFrm/ThirdFrameScripts/BoxTrigger.cs
+++ Assets/Scenes/Snd_Trd/TrdFrm/ThirdFrameScripts/BoxTrigger.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BoxTrigger : MonoBehaviour
 {
-    public static int compteurBox;
+    public static int compteurBox = 3;
+    public Transform cam;
+    public Transform player;
+    private bool levelOne;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelOne = true;
     }
 
     // Update is called once per frame
@@ -25,6 +29,24 @@ public class BoxTrigger : MonoBehaviour
         {
             Debug.Log("Toby");
             compteurBox += 1;
+        }
+        
+        if (compteurBox == 4)
+        {
+            if(levelOne)
+            {
+                cam.transform.position = new Vector3(53.6f, 16.12f, -19.75f);
+                player.position = new Vector3(29.2999992f, 3.79999995f, 16.6119995f);
+                compteurBox = 0;
+                levelOne = false;
+            }
+            else
+            {
+                Debug.Log("Caca");
+                compteurBox = 0;
+                PlayerPrefs.SetInt("level3", 1);
+                SceneManager.LoadScene("Corridor");
+            }
         }
     }
     private void OnTriggerExit(Collider other)
